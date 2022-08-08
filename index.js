e2af9b2b50918a165c0a01d5a297f86e18987968
ivanlyutov17 <lyutov17@yandex.ru>
2022-08-08 19:21:48 +0200

    test

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+
+var morgan = require('morgan')
+const app = express();
+
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(express.urlencoded({extended: false}));
+
+let postData = {};
+let posts = [];
+
+app.listen(PORT,err=>{
+    err?console.log(err):console.log('Listening on port: '+PORT);
+})
+
+app.get('/',(req,res)=>{
+    const title = 'Home'
+    res.render(path.join(__dirname, 'static','index.ejs'),{title});
+})
+app.get('/posts',(req,res)=>{
+    const title = 'Posts'
+    posts.push(postData);
+    res.render(path.join(__dirname, 'static','posts.ejs'),{title,posts});
+})
+app.post('/newPost',(req,res)=>{
+    const { title, author, text } = req.body;
+    postData = {
+      id: new Date(),
+      date: (new Date()).toLocaleDateString(),
+      title,
+      author,
+      text,
+    };
+    res.render(path.join(__dirname, 'static','newPost.ejs'), { title });
+})
+app.get('/newPost',(req,res)=>{
+    const title = 'Add post'
+
+    res.render(path.join(__dirname, 'static','newPost.ejs'),{title});
+})
+
+app.get('/contact',(req,res)=>{
+    const title = 'Contact'
+
+    res.render(path.join(__dirname, 'static','contact.ejs'),{title});
+})
+
+app.use((req,res)=>{    
+    res.
+    status(404).
+    sendFile(path.join(__dirname, 'static','error.ejs'));
+})
