d149d79460703c950611653ba12e600a7249fd85
ivanlyutov17 <lyutov17@yandex.ru>
2022-08-09 15:48:10 +0200

    added database.connected.realised post and delete methods

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
 const validator = require('./middlewares');
+const mongoose = require('mongoose');
+const Post = require('./models/post'); 
+const Contact = require('./models/contacts');
 
 var morgan = require('morgan')
 const app = express();
 
+
+
 const PORT = 3000;
+
+const database = 'mongodb+srv://ivanlyutov17:Reas2014@cluster0.az8lhgw.mongodb.net/node-blog?retryWrites=true&w=majority';
+
+mongoose.connect(database,{useNewUrlParser: true, useUnifiedTopology:true})
+.then((res) =>console.log('Connected to DB'))
+.catch((err) =>console.log(err));
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'static')));
@@ -23,36 +34,56 @@ app.get('/',(req,res)=>{
     const title = 'Home'
     res.render(path.join(__dirname, 'static','index.ejs'),{title});
 })
+
 app.get('/posts',(req,res)=>{
     const title = 'Posts'
-    res.render(path.join(__dirname, 'static','posts.ejs'),{title,posts});
+    Post
+    .find()
+    .sort({createdAt:-1})
+    .then((posts)=>res.render(path.join(__dirname, 'static','posts.ejs'),{title,posts}))
+    .catch((err)=>console.log(err));
 })
+
+app.delete('/posts/:id',(req,res)=>{
+    Post
+    .findByIdAndDelete(req.params.id)
+    .then((result)=>res.sendStatus(200))
+    .catch((err)=>console.log(err));
+});
+
 app.post('/newPost',(req,res)=>{
     const { title, author, text } = req.body;
-    postData = {
-      id: new Date(),
-      date: (new Date()).toLocaleDateString(),
-      title,
-      author,
-      text,
-    };
-    let page = validator(postData);
-    if (page !== 'error.ejs') posts.push(postData);
-    res.setHeader("Content-Type", "text/html");
-    page !== 'error.ejs'?    res.redirect('/posts'):
-    res.render(path.join(__dirname, 'static','error.ejs'));
-
+    const post = new Post({title, author, text});
+    post
+    .save()
+    .then(res.redirect('/posts'))
+    .catch((err)=>
+    console.log(err)
+    );
 })
+
 app.get('/newPost',(req,res)=>{
     const title = 'Add post'
 
     res.render(path.join(__dirname, 'static','newPost.ejs'),{title});
 })
 
+app.get('/posts/:id', (req, res) => {
+    const title = 'Post';
+    Post
+      .findById(req.params.id)
+      .then(post => res.render(path.join(__dirname, 'static','post.ejs'), { post, title }))
+      .catch((error) => {
+        console.log(error);
+        res.render(path.join(__dirname, 'static','error.ejs')), { title: 'Error' }});
+    });
+
+
 app.get('/contact',(req,res)=>{
     const title = 'Contact'
-
-    res.render(path.join(__dirname, 'static','contact.ejs'),{title});
+    Contact.find()
+    .then((contacts)=>res.render(path.join(__dirname, 'static','contact.ejs'),{title,contacts}))
+    .catch((err)=>console.log(err));
 })
 
 app.use((req,res)=>{    
---
+++ models/contacts.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const contactSchema = new Schema({
+    name:{
+        type:'string',
+        required:true,
+    },
+    link:{
+        type:'string',
+        required:true,
+    },
+});
+
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
